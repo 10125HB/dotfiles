5025eee7395a81e31a0acf57a2bfb528bafa797d
Hannes Basler <hannes.basler@gmail.com>
2013-01-20 20:09:10 +0100

    new prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,19 @@
-autoload -U colors && colors
-PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+# Stuff for git
+parse_git_branch () {
+    git branch 2> /dev/null | grep "*" | sed -e 's/* \(.*\)/ (\1)/g'
+}
+
+#autoload -U colors && colors
+#PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %%{$reset_color%}%% "
+
+# sunburst.vim like colors for prompt
+BLACK=$'\033[0m'
+RED=$'\033[38;5;167m'
+GREEN=$'\033[38;5;71m'
+BLUE=$'\033[38;5;111m'
+YELLOW=$'\033[38;5;228m'
+ORANGE=$'\033[38;5;173m'
+
+function precmd() {
+    export PROMPT="%{$RED%}%n@%m%{$BLACK%}:%{$GREEN%}%~%{$YELLOW%}$(parse_git_branch)%{$BLACK%}%# "
+}
